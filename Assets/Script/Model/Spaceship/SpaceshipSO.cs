7db9dcd98b7bdb1d915a594cb28897a47625e0f1
thelazyant164 <lythienan164@gmail.com>
2023-05-27 13:04:09 +1000

    Add spaceship data

--- Assets/Script/Model/Spaceship/SpaceshipSO.cs
+++ Assets/Script/Model/Spaceship/SpaceshipSO.cs
@@ -11,14 +11,16 @@ namespace Com.oHMysTArs.Spaceship
         public Pattern.Pattern Pattern;
         public bool VIP;
         public Texture2D Texture;
+        public Texture2D ScanTexture;
 
-        public static SpaceshipSO Init(string name, Pattern.Pattern pattern, bool vip, Texture2D texture)
+        public static SpaceshipSO Init(string name, Pattern.Pattern pattern, bool vip, Texture2D texture, Texture2D scanTexture)
         {
             SpaceshipSO spaceship = ScriptableObject.CreateInstance<SpaceshipSO>();
             spaceship.name = name;
             spaceship.Pattern = pattern;
             spaceship.VIP = vip;
             spaceship.Texture = texture;
+            spaceship.ScanTexture = scanTexture;
             return spaceship;
         }
     }
